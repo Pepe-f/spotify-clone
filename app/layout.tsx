61dbe73abd744e107e97ca3a98bf29686b3d25f4
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-08-12 17:49:28 +0300

    Upload modal and functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Figtree } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import Sidebar from '@/components/Sidebar';
 import SupabaseProvider from '@/providers/SupabaseProvider';
 import UserProvider from '@/providers/UserProvider';
 import ModalProvider from '@/providers/ModalProvider';
 import ToasterProvider from '@/providers/ToasterProvider';
 
-const font = Figtree({ subsets: ['latin'] });
+const font = Inter({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata: Metadata = {
   title: 'Spotify Clone',
