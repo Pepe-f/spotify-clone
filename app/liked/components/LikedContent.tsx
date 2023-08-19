9235ba6244009cc6c6aa7490ce4c3415013a98f9
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-08-19 17:53:47 +0300

    Favorites functionality

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,14 +1,26 @@
 'use client';
 
 import { Song } from '@/types';
+import { useRouter } from 'next/navigation';
+import { useUser } from '@/hooks/useUser';
+import { useEffect } from 'react';
 import MediaItem from '@/components/MediaItem';
 import LikeButton from '@/components/LikeButton';
 
-interface SearchContentProps {
+interface LikedContentProps {
   songs: Song[];
 }
 
-const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/');
+    }
+  }, [isLoading, router, user]);
+
   if (songs.length === 0) {
     return (
       <div
@@ -21,17 +33,17 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           text-neutral-400
         "
       >
-        No songs found.
+        No liked songs.
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col gap-y-2 w-full px-6">
+    <div className="flex flex-col gap-y-2 w-full p-6">
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem data={song} onClick={() => {}} />
+            <MediaItem onClick={() => {}} data={song} />
           </div>
           <LikeButton songId={song.id} />
         </div>
@@ -40,4 +52,4 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
   );
 };
 
-export default SearchContent;
+export default LikedContent;
